3cfaf669ecdeabb7fa8fc936cc5f516d3ebc7d27
Clément Chausson <clement.chausson@epitech.eu>
2022-05-06 16:58:31 +0200

    [+] Cmake with raylib, but we gotta have encapsulation

--- cross_platform/src/main.cpp
+++ cross_platform/src/main.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
+#include "../_deps/raylib-src/src/raylib.h"
 
-int main()
+int main(void)
 {
-    std::cout << "Hello, World!" << std::endl;
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+    CloseWindow();
+
     return 0;
 }
