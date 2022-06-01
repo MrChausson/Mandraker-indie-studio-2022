b8b88767876f902a44c6a77b57165cd80b7f1f28
clement de laage de meux <clement.de-laage-de-meux@epitech.eu>
2022-06-01 07:03:02 +0200

    [+]add many function asked, on basic, just simple tranaltion, have to check if everything works fine

--- src/raylib_encap/raylib.cpp
+++ src/raylib_encap/raylib.cpp
@@ -15,7 +15,6 @@ raylib::raylib()
 
 raylib::~raylib()
 {
-    CloseWindow();
 }
 
 void Raylib::Windowsinit(int &screenWidth, int &screenHeight, std::string const &title, std::size_t const &fps) noexcept
@@ -32,7 +31,7 @@ void Raylib::GetScreenWidth() const noexcept
     x_y_screen.first = GetScreenWidth()
 }
 
-void Raylib::GetScreenHeight()) const noexcept
+void Raylib::GetScreenHeight() const noexcept
 {
     x_y_screen.second = GetScreenHeight()
 }
@@ -104,9 +103,9 @@ Vector2 Raylib::GetMousePos() noexcept
     return GetMousePosition();
 }
 
-void Raylib::SetMousePos(int x, int y) const noexcept
+void Raylib::SetMousePos(std::pair<int, int> const &pos) const noexcept
 {
-    return SetMousePosition(x, y);
+    return SetMousePosition(pos.first, pos.second);
 }
 
 int GetKeyPres(void) noexcept
@@ -157,4 +156,143 @@ void SeekMStream(Music music, float position) noexcept
 void SetMVolume(Music music, float volume) noexcept
 {
     return SetMusicVolume(music, volume);
+}
+
+void BeginM3D(Camera3D camera) noexcept
+{
+    return BeginMode3D(camera);
+}
+
+void EndM3D(void) noexcept
+{
+    return EndMode3D();
+}
+
+void BeginTextMode(RenderTexture2D target) noexcept
+{
+    return BeginTextureMode(target);
+}
+
+void EndTextMode(void) noexcept
+{
+    return EndTextureMode();
+}
+
+void BeginShadMode(Shader shader) noexcept
+{
+    return BeginShaderMode(shader);
+}
+
+void EndShadMode(void) noexcept
+{
+    return EndShaderMode();
+}
+
+void BeginBMode(int mode) noexcept
+{
+    return BeginBlendMode(mode);
+}
+
+void EndBMode(void) noexcept
+{
+    return EndBlendMode();
+}
+
+bool Raylib::checkCollision(std::pair<float, float> mouse_xy, float width, float height, float X, float Y)
+{
+    Rectangle rect = {posX, posY, width, height};
+    Vector2 vec = {pos.first, pos.second};
+
+    return (CheckCollisionPointRec(vec, rect));
+}
+
+Sound Rayib::LSound(std::string fileName) noexcept
+{
+    return Loadsound(fileName.c_str());
+}
+
+Font Rayib::LFontEx(std::string fileName, int fontSize, int *fontChars, int glyphCount) noexcept
+{
+    return LoadFontEx(fileName.c_str(), fontSize, fontChars, glyphCount);
+}
+
+Texture2D Rayib::LTexture(std::string fileName) noexcept
+{
+    return LoadTexture(filename.c_str());
+}
+
+void Rayib::PlayS(Sound sound) noexcept
+{
+    return PlaySound(sound);
+}
+
+void Rayib::StopS(Sound sound) noexcept
+{
+    return StopSound(sound);
+}
+
+void Rayib::PauseS(Sound sound) noexcept
+{
+    return PaueSound(sound);
+}
+
+void Rayib::ResumeS(Sound sound) noexcept
+{
+    return ResumeSound(sound);
+}
+
+void Rayib::ClearBkground(Color color) noexcept
+{
+    return ClearBackground(color);
+}
+
+void Rayib::BgDrawing(void) noexcept
+{
+    return BeginDrawing();
+}
+
+void Rayib::EDrawing(void) noexcept
+{
+    return EndDrawing();
+}
+
+void Rayib::DwTexture(Texture2D texture, std::pair<int, int> conts &pos, Color tint) noexcept
+{
+    return DrawTexture(texture, pos.first, pos.second, tint);
+}
+
+void Rayib::DwFPS(std::pair<int, int> const &pos) noexcept
+{
+    return DrawFPS(pos.first, pos.second);
+}
+
+void Rayib::DwText(std::string text, std::pair<int, int> const &pos, float fontSize, Color color) noexcept
+{
+    return DrawText(text.c_str(), pos.first, pos.second, fontSize, color);
+}
+
+void Rayib::DwTextEx(Font font, std::string text, Vector2 position, float fontSize, float spacing, Color tint) noexcept
+{
+    return DrawTextEx(font, text.c_str(), position, fontSize, spacing, tint);
+}
+
+
+void Rayib::UpdtMusicStream(Music music) noexcept
+{
+    return UpdateMusicStream(music);
+}
+
+void Rayib::UnlTexture(Texture2D texture) noexcept
+{
+    return UnloadTexture(texture);
+}
+
+void Rayib::UnlFont(Font font) noexcept
+{
+    return UnloadFont(font);
+}
+
+void Rayib::UnlSound(Sound sound) noexcept
+{
+    return UnloadSound(sound);
 }
