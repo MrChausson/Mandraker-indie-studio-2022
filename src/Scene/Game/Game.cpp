00ecacf90d0ae9087b91cd17aac7eb248b34b045
antoine.descamps@epitech.eu <antoine.descamps@epitech.eu>
2022-06-07 11:39:57 +0200

    [-] comment in game.cpp and ([+] bomberman in gitignore

--- src/Scene/Game/Game.cpp
+++ src/Scene/Game/Game.cpp
@@ -71,33 +71,3 @@ Game::~Game()
 {
     std::cout << "Game destructor" << std::endl;
 }
-
-
-
-// Model model = LoadModel("assets/models/mcg/mcg.iqm");
-//     Texture2D texture = LoadTexture("assets/models/mcg/c_McGonagall_Body_Diffuse_v1@4x.png");
-//     Texture2D texture1 = LoadTexture("assets/models/mcg/c_McGonagall_eyes_Diffuse_v1@4x.png");
-//     Texture2D texture2 = LoadTexture("assets/models/mcg/c_McGonagall_hands_Diffuse_v1@4x.png");
-//     Texture2D texture3 = LoadTexture("assets/models/mcg/c_McGonagall_Hat_Diffuse_v1@4x.png");
-//     Texture2D texture4 = LoadTexture("assets/models/mcg/c_McGonagall_Head_Diffuse_v1@4x.png");
-//     Texture2D texture5 = LoadTexture("assets/models/mcg/glass.png");
-
-//     SetMaterialTexture(&model.materials[0], MATERIAL_MAP_DIFFUSE, texture);
-//     SetMaterialTexture(&model.materials[1], MATERIAL_MAP_DIFFUSE, texture1);
-//     SetMaterialTexture(&model.materials[2], MATERIAL_MAP_DIFFUSE, texture2);
-//     SetMaterialTexture(&model.materials[3], MATERIAL_MAP_DIFFUSE, texture3);
-//     SetMaterialTexture(&model.materials[4], MATERIAL_MAP_DIFFUSE, texture4);
-//     SetMaterialTexture(&model.materials[5], MATERIAL_MAP_DIFFUSE, texture5);
-
-//     SetModelMeshMaterial(&model, 1, 0); // Robe
-//     SetModelMeshMaterial(&model, 2, 1); // Yeux
-//     SetModelMeshMaterial(&model, 3, 2);  // Main
-//     SetModelMeshMaterial(&model, 5, 3); // Chapeau
-//     SetModelMeshMaterial(&model, 0, 4); // tete
-//     SetModelMeshMaterial(&model, 4, 5); // Cheveux
-
-//     Vector3 position = { 0.0f, 0.0f, 0.0f };
-
-//     unsigned int animsCount = 0;
-//     ModelAnimation *anims = LoadModelAnimations("assets/models/mcg/mcg.iqm", &animsCount);
-//     int animFrameCounter = 0;
