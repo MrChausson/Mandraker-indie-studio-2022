5dd116b43d92e3f153099d29885d8f00212a3ed7
antoine.descamps@epitech.eu <antoine.descamps@epitech.eu>
2022-06-08 10:44:14 +0200

    [+] music in the splashscreen

--- src/Scene/SplashScreen/SplashScreen.cpp
+++ src/Scene/SplashScreen/SplashScreen.cpp
@@ -18,16 +18,21 @@ SplashScreen::SplashScreen(Engine *engine)
     this->_ecsManager = std::make_unique<ECSManager>();
     Menu *menu = new Menu();
     
-    int bg = this->_ecsManager->createEntity();
     int load_bar = this->_ecsManager->createEntity();
+    int music_id = this->_ecsManager->createEntity();
 
     Texture2D back = LoadTexture("assets/materials/loading_bg.png");
     Texture2D load = LoadTexture("assets/materials/loading.png");
+    Music music = LoadMusicStream("assets/sounds/load_bg.mp3");
+
+    PlayMusicStream(music);
     
     this->_ecsManager->addComponent(load_bar, std::make_unique<Placable>(back.width/2, (back.height/20) * 17));
     this->_ecsManager->addComponent(load_bar, std::make_unique<Loadable>(load, back, 0.0f, menu->getECS(), 5));
+    this->_ecsManager->addComponent(music_id , std::make_unique<Musicable>(music));
 
     this->_ecsManager->addSystem(std::make_unique<Loading>());
+    this->_ecsManager->addSystem(std::make_unique<Music_sys>());
 }
 
 SplashScreen::~SplashScreen()
