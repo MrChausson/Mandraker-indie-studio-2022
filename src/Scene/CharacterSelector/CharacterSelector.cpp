6adb4d02fedf9ac19aedfc4ac364458a2a735750
Hugo Delille <hugo.delille@epitech.eu>
2022-06-08 15:57:35 +0200

    [~] Fix de la caméra dans le jeu

--- src/Scene/CharacterSelector/CharacterSelector.cpp
+++ src/Scene/CharacterSelector/CharacterSelector.cpp
@@ -10,9 +10,17 @@
 #include "../../ecs/Components/Clickable/Clickable.hpp"
 #include "../../Tools/Button.hpp"
 #include <string>
+#include "../../ecs/Components/CameraComponent/CameraComponent.hpp"
+#include "../../ecs/Components/Animable/Animable.hpp"
+#include "../../ecs/Systems/Animation/Animation.hpp"
 
 CharacterSelector::CharacterSelector(Engine *engine)
 {
+    Vector3 position = { 90.0f, 1.0f, 0.0f };
+    Vector3 target = { 0.0f, 0.0f, 0.0f }; //  Gauche-Droite|Haut-bas|??
+    Vector3 up = { 0.0f, 1.0f, 0.0f };
+    Vector3 scale = { 0.5, 0.5, 0.5 };
+
     std::cout << "Options creating" << std::endl;
     this->_ecsManager = std::make_unique<ECSManager>();
     int bg_id = this->_ecsManager->createEntity();
@@ -23,9 +31,10 @@ CharacterSelector::CharacterSelector(Engine *engine)
     int bg_perso2 = this->_ecsManager->createEntity(); 
     int bg_perso3 = this->_ecsManager->createEntity(); 
     int bg_perso4 = this->_ecsManager->createEntity(); 
+    int character_mcg = this->_ecsManager->createEntity();
+    int camera = this->_ecsManager->createEntity();
 
-
-    int total = 350 * 4;
+    int total = 400 * 4;
     int start_pos = (1920/2 - total/2);
 
     this->_music = LoadMusicStream("assets/sounds/menu_bg.mp3");
@@ -38,34 +47,52 @@ CharacterSelector::CharacterSelector(Engine *engine)
     this->_btn_textures[2] = LoadTexture("assets/materials/buttons/btn_clicked.png");
     this->_box_texture = LoadTexture("assets/materials/selection/btn_inactive.png");
 
+    // Creating vector texture and the mesh order for mcg 
+    std::vector<Texture2D> textures_mcg = {
+        LoadTexture("assets/models/mcg/c_McGonagall_Body_Diffuse_v1@4x.png"),
+        LoadTexture("assets/models/mcg/c_McGonagall_eyes_Diffuse_v1@4x.png"),
+        LoadTexture("assets/models/mcg/c_McGonagall_hands_Diffuse_v1@4x.png"),
+        LoadTexture("assets/models/mcg/c_McGonagall_Hat_Diffuse_v1@4x.png"),
+        LoadTexture("assets/models/mcg/c_McGonagall_Head_Diffuse_v1@4x.png"),
+        LoadTexture("assets/models/mcg/glass.png")
+    };
+    std::vector<int> meshOrder_mcg = {
+        1, 2, 3, 5, 0, 4
+    };
+    Vector3 rotationAxis = {2.0f, 0.0f, 0.0f};
+
     PlayMusicStream(this->_music);
 
     this->_ecsManager->addComponent(title_id, std::make_unique<Placable>(550, 50));
     this->_ecsManager->addComponent(title_id, std::make_unique<DrawableSprite>(this->_title_texture, 1));
 
     this->_ecsManager->addComponent(title_text, std::make_unique<Placable>(700, 100));
-    this->_ecsManager->addComponent(title_text, std::make_unique<DrawableText>(2,"SELECTION DE PERSONNAGE", Color{255, 255, 255, 255}, this->_btn_font));
+    this->_ecsManager->addComponent(title_text, std::make_unique<DrawableText>(2,"choose your character", Color{255, 255, 255, 255}, this->_btn_font));
 
     this->_ecsManager->addComponent(bg_id, std::make_unique<Placable>(0, 0));
     this->_ecsManager->addComponent(bg_id, std::make_unique<DrawableSprite>(this->_background_texture, 0));
 
-
-
     this ->_ecsManager->addComponent(bg_perso1, std::make_unique<Placable>(start_pos, 200));
     this->_ecsManager->addComponent(bg_perso1, std::make_unique<DrawableSprite>(this->_box_texture, 1));
 
-    this ->_ecsManager->addComponent(bg_perso2, std::make_unique<Placable>(start_pos + (350*1), 200));
+    this ->_ecsManager->addComponent(bg_perso2, std::make_unique<Placable>(start_pos + (400*1), 200));
     this->_ecsManager->addComponent(bg_perso2, std::make_unique<DrawableSprite>(this->_box_texture, 1));
     
-    this ->_ecsManager->addComponent(bg_perso3, std::make_unique<Placable>(start_pos + (350*2), 200));
+    this ->_ecsManager->addComponent(bg_perso3, std::make_unique<Placable>(start_pos + (400*2), 200));
     this->_ecsManager->addComponent(bg_perso3, std::make_unique<DrawableSprite>(this->_box_texture, 1));
     
-    this ->_ecsManager->addComponent(bg_perso4, std::make_unique<Placable>(start_pos + (350*3), 200));
+    this->_ecsManager->addComponent(bg_perso4, std::make_unique<Placable>(start_pos + (400*3), 200));
     this->_ecsManager->addComponent(bg_perso4, std::make_unique<DrawableSprite>(this->_box_texture, 1));
 
+//mcg
+    this->_ecsManager->addComponent(camera, std::make_unique<CameraComponent>(position, target, up, 45.0f, CAMERA_PERSPECTIVE));
+    this->_ecsManager->addComponent(character_mcg, std::make_unique<Placable>(0.0f, 0.0f, 0.0f, rotationAxis, -90.0f, scale));
+    Model mgmModel = LoadModel("assets/models/mcg/mcg.iqm");
+    this->_ecsManager->addComponent(character_mcg, std::make_unique<DrawableModel>(textures_mcg, mgmModel, meshOrder_mcg, 2));
+    this->_ecsManager->addComponent(character_mcg, std::make_unique<Animable>("assets/models/mcg/mcg.iqm", ANIMATION_TYPE::LOST));
 
-
-    Button(this->_ecsManager.get(), "Play", 724, 900, this->_btn_font, this->_btn_textures, SCENE_GAME, CLICKABLE_ACTION_CHANGE_ECS);
+//button play for play
+    Button(this->_ecsManager.get(), "confirm", 724, 900, this->_btn_font, this->_btn_textures, SCENE_GAME, CLICKABLE_ACTION_CHANGE_ECS);
 
     this->_ecsManager->addComponent(music_id, std::make_unique<Musicable>(this->_music));
 
