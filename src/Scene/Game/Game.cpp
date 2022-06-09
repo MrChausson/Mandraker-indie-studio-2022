685b73565127cbc75a93b8a47c6d214be7196651
Clément Chausson <clement.chausson@epitech.eu>
2022-06-09 16:03:11 +0200

    [+] Added random gnomes

--- src/Scene/Game/Game.cpp
+++ src/Scene/Game/Game.cpp
@@ -16,6 +16,8 @@
 #include "../../ecs/Components/Drawable/DrawableCube.hpp"
 #include "../../ecs/Components/Drawable/DrawableCubeTexture.hpp"
 
+float mapHeight = 14;
+float mapWidth = 10;
 
 Game::Game()
 {
@@ -170,18 +172,25 @@ Game::~Game()
 void Game::loadMap(std::string map_src)
 {
     Raylib::Raylib_encap Raylib_encp;
-    //load map from file
     std::ifstream myfile (map_src);
     std::string line;
     int i = 0;
+
+    // Gass
     Texture2D grass_texture = Raylib_encp.LTexture("assets/materials/grass.png");
+    // Stone
+    Texture2D stone_texture = Raylib_encp.LTexture("assets/materials/game/stone.png");
     std::vector<Texture2D> textures_pot = {
         Raylib_encp.LTexture("assets/models/bag/p_FertiliserBag_Diffuse_v1@4x.png")
     };
+
+    //bag
     std::vector<int> texture_po_mesh_order = {
         0
     };
-    //Table model
+    Model bagModel = Raylib_encp.LModel("assets/models/bag/bag.obj");
+
+    //Table
     std::vector<Texture2D> textures_tables = {
         Raylib_encp.LTexture("assets/models/table/leaves2_v73@4x.png"),
         Raylib_encp.LTexture("assets/models/table/Metal_potteryHolders_v11@4x.png"),
@@ -193,10 +202,24 @@ void Game::loadMap(std::string map_src)
     Model tableModel = Raylib_encp.LModel("assets/models/table/table.obj");
     Model tableModelRotate = Raylib_encp.LModel("assets/models/table/table.obj");
     tableModelRotate.transform = MatrixRotateY(1.55 );
-    Model bagModel = Raylib_encp.LModel("assets/models/bag/bag.obj");
+
+    // Gnome
+    Model gnome = Raylib_encp.LModel("assets/models/gnome/gnome.iqm");
+    gnome.transform = MatrixRotateX(1.55);
+    std::vector<Texture2D> textures_gnome = {
+        Raylib_encp.LTexture("assets/models/gnome/gnome_DIF_v1@4x.png"),
+        Raylib_encp.LTexture("assets/models/gnome/GnomeHoles_DIF_v1@4x.png")
+    };
+    std::vector<int> texture_gnome_mesh_order = {
+        0, 2
+    };
+    Vector3 gnome_scale = {0.015, 0.015, 0.015};
+    // 
     Vector3 zeroVector3 = {0.0f, 0.0f, 0.0f};
+    Vector3 rotation_gnome = {0.0f, 90.0f, 0.0f};
     Vector3 bag_scale = {0.03, 0.03, 0.03};
     Vector2 size = { 1, 1 };
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
     if (!myfile.is_open())
         throw Error_file("Error while opening map file");
@@ -207,16 +230,27 @@ void Game::loadMap(std::string map_src)
             if (line[j] == 'r') {
                 entity->addComponent(std::make_unique<Placable>(j, 0.0f, i, zeroVector3 , -45, bag_scale));
                 entity->addComponent(std::make_unique<DrawableModel>(textures_pot, bagModel, texture_po_mesh_order));
+                // we have to put grass also
+                grass_block->addComponent(std::make_unique<Placable>(j, -1.0f, i, zeroVector3));
+                grass_block->addComponent(std::make_unique<DrawableCubeTexture>(grass_texture));
             } else if (line[j] == 'B') {
                 entity->addComponent(std::make_unique<Placable>(j, 0.5f, i, zeroVector3));
                 entity->addComponent(std::make_unique<DrawableModel>(textures_tables, tableModel, texture_table_mesh_order));
+                // we have to put grass also
+                grass_block->addComponent(std::make_unique<Placable>(j, -1.0f, i, zeroVector3));
+                grass_block->addComponent(std::make_unique<DrawableCubeTexture>(grass_texture));
             } else if (line[j] == '*') {
-                entity->addComponent(std::make_unique<Placable>(j, 0.0f, i, zeroVector3));
-                entity->addComponent(std::make_unique<DrawablePlane>(size));
+                entity->addComponent(std::make_unique<Placable>(j, -1.0f, i, zeroVector3));
+                entity->addComponent(std::make_unique<DrawableCubeTexture>(stone_texture));
+            } else {
+                if (std::rand() % 2 == 1) { // 50% chance to spawn a gnome
+                    entity->addComponent(std::make_unique<Placable>(j, -0.7, i, zeroVector3, 0, gnome_scale));
+                    entity->addComponent(std::make_unique<DrawableModel>(textures_gnome, gnome, texture_gnome_mesh_order));
+                }
+                // we have to put grass also
+                grass_block->addComponent(std::make_unique<Placable>(j, -1.0f, i, zeroVector3));
+                grass_block->addComponent(std::make_unique<DrawableCubeTexture>(grass_texture));
             }
-            // we have to push grass also
-            grass_block->addComponent(std::make_unique<Placable>(j, -1.0f, i, zeroVector3));
-            grass_block->addComponent(std::make_unique<DrawableCubeTexture>(grass_texture));
             this->_mapEntities->push_back(entity);
         }
         i++;
