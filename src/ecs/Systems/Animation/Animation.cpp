3598b2f5651e1bd7e2441bc5cbaa94fa29aff54f
antoine.descamps@epitech.eu <antoine.descamps@epitech.eu>
2022-06-11 11:05:40 +0200

    [+] clock for animation

--- src/ecs/Systems/Animation/Animation.cpp
+++ src/ecs/Systems/Animation/Animation.cpp
@@ -11,6 +11,7 @@
 
 Animation::Animation()
 {
+    this->_clock = std::chrono::system_clock::now();
 }
 
 Animation::~Animation()
@@ -28,8 +29,12 @@ void Animation::apply(std::vector<IComponent *> component)
     Raylib::Raylib_encap Raylib_encp;
     Animable *animable = static_cast<Animable *>(component[0]);
     DrawableModel *model = static_cast<DrawableModel *>(component[1]);
-
-    animable->setAnimFrameCounter(animable->getAnimFrameCounter() + 1);
+    std::chrono::duration<double> elapsedSecond = std::chrono::system_clock::now() - this->_clock;
+    std::cout << "elaps  " << elapsedSecond.count() << std::endl;
+    if (elapsedSecond.count() > 0.0003) {
+        animable->setAnimFrameCounter(animable->getAnimFrameCounter() + 1);
+        this->_clock = std::chrono::system_clock::now();
+    }
     Raylib_encp.UpdtModelAnimation(model->getModel(), animable->getAnims()[animable->getAnimationType()], animable->getAnimFrameCounter());
     if (animable->getAnimFrameCounter() >= animable->getAnims()[animable->getAnimationType()].frameCount)
         animable->setAnimFrameCounter(0);
