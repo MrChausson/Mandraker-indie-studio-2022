9db4c1626f5fbcc923e4ba6c6af4cf895b100ece
Ritoche1 <72013084+Ritoche1@users.noreply.github.com>
2022-06-11 16:22:02 +0200

    [+] sound for the mandrake plant wwhen press space

--- src/ecs/Systems/Player/Player.cpp
+++ src/ecs/Systems/Player/Player.cpp
@@ -27,14 +27,22 @@ Player::Player(ECSManager *ecsManager)
        1, 2
     };
     this->_scaleMandrake = {0.0002f, 0.0002f, 0.0002f};
+    this->_plantSound = Raylib_encp.LSound("assets/sounds/mandrake/potting.mp3");
 }
 
 Player::~Player()
 {
+    Raylib::Raylib_encap Raylib_encp;
+    UnloadModel(this->_mandrakeModel);
+    Raylib_encp.UnlSound(this->_plantSound);
+    Raylib_encp.UnlTexture(this->_texturesMandrake[0]);
+    Raylib_encp.UnlTexture(this->_texturesMandrake[1]);
+
 }
 
 void Player::apply(std::vector<IComponent *> component)
 {
+    Raylib::Raylib_encap Raylib_encp;
     Placable *playerPlace = static_cast<Placable *>(component[0]);
     Movable *playerMove = static_cast<Movable *>(component[1]);
     Vector3 playerPos = {1.0f, 0.0f, 0.0f};
@@ -42,6 +50,7 @@ void Player::apply(std::vector<IComponent *> component)
     int bomb_id;
     if (type == MOVABLE_PLAYER && IsKeyPressed(KEY_SPACE)) {
         bomb_id = this->_ecsManager->createEntity();
+        Raylib_encp.PlayS(this->_plantSound);
         this->_ecsManager->addComponent(bomb_id, std::make_unique<Placable>(playerPlace->getX(), playerPlace->getY(), playerPlace->getZ(), playerPos, -90.0f, this->_scaleMandrake));
         this->_ecsManager->addComponent(bomb_id, std::make_unique<DrawableModel>(this->_texturesMandrake, this->_mandrakeModel, this->_meshOrderMandrake));
         this->_ecsManager->addComponent(bomb_id, std::make_unique<Animable>("assets/models/mandrake/mandrake.iqm", ANIMATION_TYPE::IDLE));
